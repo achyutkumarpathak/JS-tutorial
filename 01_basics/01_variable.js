4e43c14c3cc516a9d8f43c55c44bcdf14a0388ba
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-09 13:45:21 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const account_id = 144553
+let accountemail = "achyut@gmail.com"
+var accountPassword = "123456"// not preferd to use var because of issue in block scope and functional scope
+accountCity = "jamshedpur"
+
+account_id = 134567 // changes not allowed
+accountemail= "achyutkumarpathak07@gmial.com"
+accountCity = "banglore"
+accountPassword = "123456789"
+
+console.table([account_id,accountemail, accountPassword, accountCity]) // used to print the data in table form 
