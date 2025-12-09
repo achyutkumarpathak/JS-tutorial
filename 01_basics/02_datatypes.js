b1418e494bb1854e1ee2bb672ad9d8ba338232a7
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-09 15:31:46 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)  we are using nodejs not browser
+let name = "Achyut"
+let age = 20
+let isloggin = false
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+object
+*/
+
+console.log(typeof("achyut"))
