bdcaa96e6550ff307741365d7d31093f510a9609
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-10 12:06:15 +0000

    added comparison file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) //true
+
+console.log(undefined >0) // false
+console.log(undefined ==0)// false
+console.log(undefined <0)// false
+
+// === this operator check the values and datatypes also
+console.log("2"==2); // it check only values
+console.log("2"===2); // its checks values and datatypes both
