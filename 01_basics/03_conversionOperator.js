7a9c0f8fca8285d68a9253a10e475a7cba69c5d4
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-10 05:40:00 +0000

    container config done

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -24,3 +24,49 @@ let someNumber = 22
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log( typeof stringNumber)
+
+// ************Operations************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+// console.log(2/2)
+
+let str1 = "hello"
+let str2 = " baby"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2) // =>12
+console.log(1+"2")// => 12
+console.log("1"+2+2)// =>122
+console.log(1+2+"2") // =>32
+
+console.log(true) // true
+console.log(+true)// 1
+// console.log(true+)//  error
+console.log(+"") // 0
+
+let num1 , num2 , num3
+ num1 = num2 = num3= 2+2
+  let gameCounter = 100
+  ++gameCounter; 
+  gameCounter++;
+  console.log(gameCounter)
+
+  let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
