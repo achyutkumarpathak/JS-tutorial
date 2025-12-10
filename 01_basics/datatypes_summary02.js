3d2d644e53a7750e983038de375d92c9f51a2d16
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-10 16:20:15 +0000

    container config done

--- 01_basics/datatypes_summary02.js
+++ 01_basics/datatypes_summary02.js
@@ -30,4 +30,24 @@ let myobj = {
 const myFuction = function(){
     console.log("hello world")
 }
+ /*
+heap memory is used by non-primitive datatype => we will get the original value or reference
 
+stack memory is used by primitive datatype => we get the copy of the variable
+ */
+
+let myYoutubename = "achyutkumarpathak"
+
+let anothername = myYoutubename
+anothername= "pathakengenier"
+console.log(myYoutubename)
+console.log(anothername)
+
+let userone = {
+    email: "achyutkumarpathak07@gmail.com",
+    upi: "8434738@psbi"
+}
+let usertwo = userone
+userone.email = "nitupathak@gmail.com"
+console.log(userone.email)
+console.log(usertwo.email)
