5a060d510e32119bb026e41c5ad44fb20c1c0497
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-14 10:16:20 +0000

    container config done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,47 @@ console.log(a)
 // console.log(c) // here c is accessible outside the scope which will create problem so that why we don't use var
 
 // the scope in terminal thorugh node is different and in console is different
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){// In this type we can decalare function name before executing
+    return num + 1
+}
+
+
+
+addTwo(5)// here it give an error showing that function is accessing without instiliation bcs we made a funciootn and store in a variable
+const addTwo = function(num){
+    return num + 2
+}
