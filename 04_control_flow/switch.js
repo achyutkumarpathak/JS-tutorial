0efcedef9270b83449e030f0be5837bb28102fa9
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-19 13:24:48 +0000

    container config done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+const month2 = 3
+
+switch (month2) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
