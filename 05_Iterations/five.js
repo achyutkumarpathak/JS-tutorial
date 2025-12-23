c0c61976c817009e1ffe53884b775c1b2bc0181f
achyut kumar pathak <achyutkumarpathak07@gmail.com>
2025-12-23 16:58:21 +0000

    container config done

--- 05_Iterations/five.js
+++ 05_Iterations/five.js
@@ -1,3 +1,43 @@
 const coding = ["js", "ruby", "java", 
     "python","cpp"]
+// For Each Loop
+// coding.forEach(function(item){
+//     console.log(item)
+// })
+
+// Arrow Function
+// coding.forEach((item)=>{
+//     console.log(item);
     
+// })
+
+function printMe(item){
+    // console.log(item)
+}
+    
+coding.forEach(printMe) // yaha pr reference dena h na ki function dena hai like printMe()
+
+coding.forEach((item, index , arr) => {
+    // console.log(item, index, arr)
+})
+
+
+const mycoding = [
+    {
+        languagename : "javascript",
+        languageFilename: "js"
+    },
+    {
+        languagename:"python",
+        languageFilename : "py"
+    },
+    {
+        languagename: "java",
+        languageFilename: "java"
+    },
+]
+
+mycoding.forEach((val)=>{
+    console.log(val.languagename)// it will print the key
+    console.log(val.languageFilename)// it will print the value
+})
